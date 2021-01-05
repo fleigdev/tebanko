7e9d75af1a3d7d5b1ef39a114e02d8ea6769962e
Martin <fleigdev@gmail.com>
2021-01-05 18:01:31 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Footer,
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import { BootstrapVue } from 'bootstrap-vue'
 Vue.config.productionTip = false
 
+
+Vue.use(BootstrapVue)
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
