214d69655724f2b06c6893c74119e287705878b1
fleigdev <fleigdev@gmail.com>
2021-01-06 02:23:24 -0400

    jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
+    <Navbar />
+    <LandingPage />
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Bienvenidos a TeBanko" />
+    <img src="./assets/img/logo-light.png" class="l-light" height="24" alt="" />
+    <img
+      src="./assets/img/logo-tebanko.png"
+      class="l-light"
+      height="24"
+      alt=""
+    />
     <Footer />
   </div>
 </template>
@@ -9,12 +18,16 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import Footer from './components/Footer.vue';
+import Navbar from './components/Navbar.vue';
+import LandingPage from './views/LandingPage.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     Footer,
+    Navbar,
+    LandingPage,
   },
 };
 </script>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,64 @@
+<script>
+/**
+ * Index-personal component
+ */
+import { ArrowUpIcon } from 'vue-feather-icons';
+
+export default {
+  data() {
+    return {};
+  },
+  components: { ArrowUpIcon },
+};
+</script>
+
+<template>
+  <div>
+    <!-- Hero Start -->
+    <section
+      class="bg-home rtl-personal-hero bg-light d-flex align-items-center"
+      style="background:url('../assets/img/tebankocover.jpg') center center"
+      id="home"
+    >
+      <div class="container">
+        <div class="row aligntoleft">
+          <div class="col-lg-8 col-md-9">
+            <div class="title-heading mt-4">
+              <h1 class="display-3 font-weight-bold mb-3">
+                Estamos contigo para alcanzar tus sueños.
+              </h1>
+              <p class="para-desc text-muted">
+                Solicita tu préstamo personal de manera rápida y segura, desde
+                la comodidad de tu hogar.
+              </p>
+              <div class="mt-4 pt-2">
+                <a class="btn btn-primary mt-2 mr-2 mouse-down"
+                  ><i class="mdi mdi-camera"></i> Solicítalo Aquí!</a
+                >
+                <br />
+              </div>
+            </div>
+          </div>
+          <!--end col-->
+        </div>
+        <!--end row-->
+      </div>
+      <!--end container-->
+    </section>
+    <!--end section-->
+    <!-- Hero End -->
+
+    <!--end section-->
+
+    <!-- Back to top -->
+    <a
+      href="javascript: void(0);"
+      class="btn btn-icon btn-soft-primary back-to-top"
+      id="back-to-top"
+      v-scroll-to="'#topnav'"
+    >
+      <arrow-up-icon class="icons"></arrow-up-icon>
+    </a>
+    <!-- Back to top -->
+  </div>
+</template>
